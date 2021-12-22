431c107a3562ed2c9efcf9c1100d7c5202ea0343
mmmm_lemon <egorproger@gmail.com>
2021-12-22 16:36:49 +1000

    Миграция, таблица orders

---
+++ database/migrations/2021_12_22_060210_create_table_orders.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('order_name');
+            $table->string('order_bootstrap_icon')->nullable();
+            $table->longText('order_desc');
+            $table->string('price_range')->nullable();
+            $table->string('time_range')->nullable();
+            $table->boolean('visibility')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_orders');
+    }
+}
